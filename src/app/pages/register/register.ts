d8f3ddb4019957b19b3e88b914eb9b2c65c9b408
Miruna Agopian <agopian.miruna@yahoo.com>
2026-08-04 21:33:24 +0200

    cteared avatar registration section

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -3,6 +3,7 @@ import { Header } from '../../shared/components/header/header';
 import { Footer } from '../../shared/components/footer/footer';
 import { Input } from '../../shared/components/input/input';
 import { Router } from '@angular/router';
+import { ThumbPosition } from '@angular/material/slider/testing';
 
 @Component({
   selector: 'app-register',
@@ -13,6 +14,18 @@ import { Router } from '@angular/router';
 export class Register {
   router = inject(Router);
   isChecked = false;
+  hideTooltip = true;
+  avatars = [
+    '/assets/img/avatars/default_user.png',
+    '/assets/img/avatars/avatar_elias_neumann.png',
+    '/assets/img/avatars/avatar_elise_roth.png',
+    '/assets/img/avatars/avatar_frederick_beck.png',
+    '/assets/img/avatars/avatar_noah_braun.png',
+    '/assets/img/avatars/avatar_sofia_mueller.png',
+    '/assets/img/avatars/avatar_steffen_hoffmann.png',
+  ];
+
+  chosenAvatar = this.avatars[0];
 
   returnToSignin() {
     this.router.navigate(['/sign-in']);
@@ -23,6 +36,19 @@ export class Register {
   }
 
   toggleCheckbox() {
-  this.isChecked = !this.isChecked;
-}
+    this.isChecked = !this.isChecked;
+  }
+
+  selectAvatar(index: number) {
+    this.chosenAvatar = this.avatars[index];
+  }
+
+  showTooltip() {
+    this.hideTooltip = false;
+
+    setTimeout(()=> {
+      this.router.navigate(['/sign-in']);
+    },1000);
+  }
+
 }
