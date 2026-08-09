5a9684631603e46f6794e79b80413a4e4e30021d
Miruna Agopian <agopian.miruna@yahoo.com>
2026-08-09 12:50:18 +0200

    added and configured Firebase SKD to the project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { environment } from '../environments/environment';
+
+const firebaseApp = initializeApp(environment.firebase);
+const db = getFirestore(firebaseApp);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+
   ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBHQXSqbswUw5l7mzqk_QEq0uC5f1LXXdA',
+    authDomain: 'dabubble-956c7.firebaseapp.com',
+    projectId: 'dabubble-956c7',
+    storageBucket: 'dabubble-956c7.firebasestorage.app',
+    messagingSenderId: '947834258240',
+    appId: '1:947834258240:web:33dbca316b5492008370a5'
+  }
+};
