567a59ca382b60817b625c70c52b2fc2b8711990
Miruna Agopian <agopian.miruna@yahoo.com>
2026-08-19 20:11:52 +0200

    added emojis to chat textarea

--- src/app/pages/workspace/workspace-chat/chat-input/chat-input.ts
+++ src/app/pages/workspace/workspace-chat/chat-input/chat-input.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, ViewEncapsulation, HostListener } from '@angular/core';
+import { PickerComponent } from '@ctrl/ngx-emoji-mart';
 
 @Component({
   selector: 'app-chat-input',
-  imports: [],
+  imports: [PickerComponent],
   templateUrl: './chat-input.html',
   styleUrl: './chat-input.scss',
+  encapsulation: ViewEncapsulation.None,
 })
-export class ChatInput {}
+export class ChatInput {
+  @ViewChild('textarea') textarea!: ElementRef<HTMLTextAreaElement>;
+  @ViewChild('pickerContainer') pickerContainer!: ElementRef;
+  @ViewChild('emojiButton') emojiButton!: ElementRef;
+  emojiPickerOpen = false;
+
+  addEmoji(event: any) {
+    this.textarea.nativeElement.value += event.emoji.native;
+    this.emojiPickerOpen = false;
+  }
+
+  toggleEmojiPicker() {
+    this.emojiPickerOpen = !this.emojiPickerOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: MouseEvent) {
+    if (!this.emojiPickerOpen) return;
+    const clickedInsidePicker = this.pickerContainer?.nativeElement.contains(event.target);
+    const clickedEmojiButton = this.emojiButton?.nativeElement.contains(event.target);
+    if (!clickedInsidePicker && !clickedEmojiButton) {
+      this.emojiPickerOpen = false;
+    }
+  }
+}
