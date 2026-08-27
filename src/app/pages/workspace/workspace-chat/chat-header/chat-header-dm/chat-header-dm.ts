0447221576b00c6011ae326c85f832056763e911
Miruna Agopian <agopian.miruna@yahoo.com>
2026-08-27 19:27:29 +0200

    added user data for header-user-dm

--- src/app/pages/workspace/workspace-chat/chat-header/chat-header-dm/chat-header-dm.ts
+++ src/app/pages/workspace/workspace-chat/chat-header/chat-header-dm/chat-header-dm.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ChatStateService } from '../../../../../core/services/chat-state.service';
+import { NgClass } from '@angular/common';
+import { AuthService } from '../../../../../core/services/auth.service';
+
 
 @Component({
   selector: 'app-chat-header-dm',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './chat-header-dm.html',
   styleUrl: './chat-header-dm.scss',
 })
-export class ChatHeaderDm {}
+export class ChatHeaderDm {
+  chatStateService = inject(ChatStateService);
+  authService = inject(AuthService);
+  selectedUser = this.chatStateService.selectedUser;
+  mode = this.chatStateService.mode;
+  currentUser = this.authService.getCurrentUserId();
+}
